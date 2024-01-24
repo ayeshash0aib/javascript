0d6044f9d55b1e573ecdcde10b9400ede6bebf8c
Student <LAB-101@aptech-msg.edu>
2024-01-24 13:01:03 +0500

    add

---
+++ session1.js
@@ -0,0 +1,34 @@
+// if else
+var a = "yes"
+if(a=="yes"){
+document.write(true)
+}
+else{document.write(false)}
+// loops
+for(i=0;i<=5;i++){
+    document.write("<br>"+"ayesha"+"<br>")
+}
+
+// let a=1;
+// while(a<=10){
+//     console.log(a);
+//     i++;
+// }
+
+// function
+// 1
+function multiply(a,b){
+    console.log(a*b)
+}
+multiply(2,3)
+// 2
+function print(name){
+    console.log(name)
+}
+print ("welcome"+"ayesha")
+//  3
+let name=prompt("enter name")
+function username(a){
+   console.log(name)
+}
+username(name)

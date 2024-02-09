306f8060f287949321dd2499f9647e110b8db5a5
Student <LAB-106@aptech-msg.edu>
2024-02-08 22:47:17 -0800

    add

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,18 +1,25 @@
 function getvalue(){
     let a=document.getElementById("search").value ;
-    // let a=document.getElementById("search").value="" ;
     console.log(a)
     // a.remove()
 
 
   const getul=  document.querySelector("ul");
-  getul.innerHTML += `<li>${a}
-  <button onclick="updateitem()">update</button>
-   <button onclick="deleteitem(this)">delete</button></li>`
-   let b=document.getElementById("search").value="" ;
+  getul.innerHTML += `
+  <div>
+  <li>${a}</li>
+  <button onclick="updateitem(this)">update</button>
+   <button onclick="deleteitem(this)">delete</button>
+   </div>
+   `
+//    to remove content from search bar
+   document.getElementById("search").value="" ;
 
 }
 function deleteitem(getobject){
-    console.log(getobject)
+    // console.log(getobject)
 getobject.parentElement.remove()
+}
+function updateitem(getobject){
+getobject.parentElement.children[0].setAttribute("contentEditable",true)
 }
